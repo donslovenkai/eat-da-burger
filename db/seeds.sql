104e5a263b0248e83d7d19a210d44a55dbf1f687
donslovenkai <don.slovenkai@gmail.com>
2017-09-26 11:36:44 -0400

    mvc folder structure and files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Macho Burger');
+INSERT INTO burgers(burger_name) VALUES ('Quiona Vegetarian Burger');
+INSERT INTO burgers(burger_name) VALUES ('Texas Bacon Burger');
+INSERT INTO burgers(burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Big Mac Hamburger');
